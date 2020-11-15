967d3f6f2d8fdba9786ef6b70b639ea340f94165
294407816@qq.com <294407816@qq.com>
2020-11-16 00:14:21 +0800

    feat: code form

--- game.js
+++ game.js
@@ -1,32 +1,27 @@
 module.exports = function (playerAction) {
-  if (['rock', 'scissor', 'paper'].indexOf(playerAction) == -1) {
-      throw new Error('invalid playerAction');
+  if (["rock", "scissor", "paper"].indexOf(playerAction) == -1) {
+    throw new Error("invalid playerAction");
   }
   // 计算电脑出的东西
   var computerAction;
-  var random = Math.random() * 3
+  var random = Math.random() * 3;
   if (random < 1) {
-      computerAction = 'rock'
-
+    computerAction = "rock";
   } else if (random > 2) {
-      computerAction = 'scissor'
-
+    computerAction = "scissor";
   } else {
-      computerAction = 'paper'
-
+    computerAction = "paper";
   }
 
   if (computerAction == playerAction) {
-      return 0;
-
+    return 0;
   } else if (
-      (computerAction == 'rock' && playerAction == 'scissor') ||
-      (computerAction == 'scissor' && playerAction == 'paper') ||
-      (computerAction == 'paper' && playerAction == 'rock')
+    (computerAction == "rock" && playerAction == "scissor") ||
+    (computerAction == "scissor" && playerAction == "paper") ||
+    (computerAction == "paper" && playerAction == "rock")
   ) {
-      return -1;
-
+    return -1;
   } else {
-      return 1;
+    return 1;
   }
-}
+};
